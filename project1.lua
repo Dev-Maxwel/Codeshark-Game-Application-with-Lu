00bf1742643403cda7783e70f553225eae868e0f
devmaxwell <developermaxwellkaranja@gmail.com>
2023-12-12 19:58:04 +0300

    first comment

--- project1.lua
+++ project1.lua
@@ -1,2 +1,4 @@
 print("First project")
 print("Second statement. My lua engine cant be recognized by my environment variables. I am trying though😂🤣")
+--first short comment
+--[[first long comment]]
